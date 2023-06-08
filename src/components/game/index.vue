dcc6c81a84d1be67436807476230b2bcdcafb3b7
aaron <zx1101zx@gmail.com>
2023-06-08 12:31:12 +0800

    add-socket

---
+++ src/components/game/index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+
+</template>
+
+<script setup>
+import { io } from "socket.io-client";
+const socket = io('http://localhost:5001');
+
+socket.on('connect', () => {
+    console.log(socket);
+    socket.emit('message', '你好嗎？');
+})
+</script>
+
+<style scoped>
+.common-layout {
+    display: none !important;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,14 @@ import { createRouter,  createWebHashHistory } from 'vue-router';
 import Home from '../components/HomePage/index.vue';
 import RoomPage from '../components/RoomPage/page.vue';
 import Room from '../components/RoomPage/Room.vue';
+import HindGame from '../components/game/index.vue';
 
 
 const routes = [
   { path: '/', component: Home },
   { path: '/room/', component: RoomPage },
   { path: '/room/:roomId', component: Room },
-
+  { path: '/hind/game', component: HindGame },
 ]
 
 const router = createRouter({
